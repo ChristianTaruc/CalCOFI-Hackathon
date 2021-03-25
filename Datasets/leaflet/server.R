e83046dd90d30531521322a0fe3743c044936728
laurenbaggett <lbaggett@ucsd.edu>
2021-03-24 20:54:39 -0700

    server.R data

--- Datasets/leaflet/server.R
+++ Datasets/leaflet/server.R
@@ -2,6 +2,13 @@ shinyServer(function (input, output) {
   output$map <- renderLeaflet({
    leaflet(krill$Abundance.per.m2) %>%
     setView(lng = -120, lat = 34, zoom = 6) %>%
-    addTiles()
+    addTiles() %<%
+    addCircleMarkers(
+      lng = ~krill$Longitude,
+      lat = ~krill$Latitude,
+      radius = ~krill$Abundance.per.m2,
+      stroke = FALSE,
+      fillOpacity = 0.5,
+    )
   })
 })
